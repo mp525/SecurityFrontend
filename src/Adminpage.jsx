af196b6187e9841f252539110b72518592bbb6cb
matti Hansen <MattiBSH>
2021-04-02 23:57:44 +0200

    works admin skeleton

--- src/Adminpage.jsx
+++ src/Adminpage.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import facade from "./apiFacade";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
-  Table
+  Table,
+  Button
 } from "react-bootstrap";
 function Adminpage() {
   const [errorAdmin, setErrorAdmin] = useState("");
   const [dataFromServer, setDataFromServer] = useState("Error!");
   const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
   const [click, setClick] = useState(false);
   useEffect(() => {
     facade
@@ -34,13 +36,20 @@ function Adminpage() {
   };
 
 return (
-  <div>
-    <h3>{dataFromServer}</h3>
+  <div align="center" className="BlueBC">
+    <h1 className="OB">Welcome to the Admin Page</h1>
+    <br/>
+    <div className="info">
+    
+    <h3>{"Welcome to"+dataFromServer.substr(16,18)}</h3>
+    <Button   onClick={submitTitle} className="myButton">Manage Users<br/></Button>
     <p>{errorAdmin}</p>
-    <button onClick={submitTitle} className="myButton"></button>
+    
+<br/>
     {facade.isAdmin().indexOf("admin") !== -1 && (
       <>
-      <Table className="table">
+      <Table striped bordered hover size="sm">
+      
         <thead>
           <tr><th>First Name</th><th>Last Name</th><th>User Name</th><th>Email</th></tr>
         </thead>
@@ -48,13 +57,15 @@ return (
          
         {
         users &&(
-          users.map((x) => {      
+          users.map((x,idx) => {      
               return (
-                  <tr key={x.firstName}>
+                  <tr key={idx}>
                   <td>{x.firstName}</td>
                   <td>{x.lastName}</td>
                   <td>{x.userName}</td>
                   <td>{x.email}</td>
+                  <td><Button variant="warning">Edit</Button>{' '}</td>
+                  <td><Button variant="danger">Delete</Button> </td>
                   </tr>
               )})
         )}
@@ -66,9 +77,41 @@ return (
                   <h1>{x.length}</h1>
               )})
         )}</h1>
+        <br/><br/>
       </Table>
       </>
     )}
+    
+   
+    </div>
+    <br/><br/><br/><br/>
+    <div className="info">
+    
+    <h3>Moderate Posts</h3>
+    <h4>Delete Posts with unwanted content</h4>
+    <Button   onClick={submitTitle} className="myButton">Manage Posts<br/></Button>
+    <Table striped bordered hover size="sm">
+    <thead>
+          <tr><th>Author Username</th><th>Content</th></tr>
+        </thead>
+    {
+        posts &&(
+          posts.map((x,idx) => {      
+              return (
+                  <tr key={idx}>
+                  <td>{x.content}</td>
+                  
+                  <td><Button variant="warning">Edit</Button>{' '}</td>
+                  <td><Button variant="danger">Delete</Button> </td>
+                  </tr>
+              )})
+        )}
+        
+   
+    <br/><br/>
+    </Table>
+  </div>
+  <br/><br/><br/><br/>
   </div>
 );
 }
