98900cb63c6ce25d928ebd31900328eb14b81453
Joakim Skaarup Stensnæs <joakim.kss@gmail.com>
2021-04-18 23:43:11 +0200

    added new 'facebook'-like design to login form.

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ import {
   Redirect,
   NavLink,
   useLocation,
- 
   useHistory,
 } from "react-router-dom";
 import Login from "./Login";
 import facade from "./apiFacade";
 import LoginForm from "./loginForm";
 import Adminpage from "./Adminpage";
-import ProfilePage from "./ProfilePage"
+import ProfilePage from "./ProfilePage";
 import FrontPage from "./FrontPage";
+import { Row, Col, Container } from "react-bootstrap";
 function App() {
   const [errorMes, setErrorMes] = useState("");
   const [loggedIn, setLoggedIn] = useState(false);
   let history = useHistory();
 
-/*   const logout = () => {
+  /*   const logout = () => {
     facade.logout();
     setLoggedIn(false);
   }; */
@@ -44,23 +44,26 @@ function App() {
   return (
     <div>
       <Header loginMsg={loggedIn ? "Logout" : "Login"} loggedIn={loggedIn} />
-      <Switch>
-        <Route exact path="/">
-         {/*  <Home /> */}
-         {loggedIn && (
-           <>
-           {history.push("/frontpage")}
-           {/* <Redirect to="/frontpage"/> */}
-           </>
-         )}
-         {!loggedIn && (
-            <LoginForm
-              errorMes={errorMes}
-              setErrorMes={setErrorMes}
-              login={login}
-            />
-          )}
-         {/* {!loggedIn ? (
+      <Container fluid>
+        <Row className="">
+          <Col className="m-5 d-flex justify-content-center">
+            <Switch>
+              <Route exact path="/">
+                {/*  <Home /> */}
+                {loggedIn && (
+                  <>
+                    {history.push("/frontpage")}
+                    {/* <Redirect to="/frontpage"/> */}
+                  </>
+                )}
+                {!loggedIn && (
+                  <LoginForm
+                    errorMes={errorMes}
+                    setErrorMes={setErrorMes}
+                    login={login}
+                  />
+                )}
+                {/* {!loggedIn ? (
             <LoginForm
               errorMes={errorMes}
               setErrorMes={setErrorMes}
@@ -75,32 +78,39 @@ function App() {
               <button onClick={logout}>Logout</button>
             </div>
           )} */}
-        </Route>
-        <Route path="/page1">
-          {/* Change */}
-          <FetchDefault />
-        </Route>
-        <Route path="/frontpage">
-          <FrontPage />
-        </Route>
-        <Route path="/page3">
-          {/* <User /> */}
-          <ProfilePage />
-        </Route>
-        <Route path="/page4">
-          {/* <Admin /> */}
-          <Adminpage />
-        </Route>
-        <Route path="/logout">
-          <Login loggedIn={loggedIn} loginMsg={loggedIn ? "Logout" : "Login"} setLoginStatus={setLoginStatus}/>
-        </Route>
-        {/* <Route path="/login">
+              </Route>
+              <Route path="/page1">
+                {/* Change */}
+                <FetchDefault />
+              </Route>
+              <Route path="/frontpage">
+                <FrontPage />
+              </Route>
+              <Route path="/page3">
+                {/* <User /> */}
+                <ProfilePage />
+              </Route>
+              <Route path="/page4">
+                {/* <Admin /> */}
+                <Adminpage />
+              </Route>
+              <Route path="/logout">
+                <Login
+                  loggedIn={loggedIn}
+                  loginMsg={loggedIn ? "Logout" : "Login"}
+                  setLoginStatus={setLoginStatus}
+                />
+              </Route>
+              {/* <Route path="/login">
           
         </Route> */}
-        <Route path="*">
-          <NoMatch />
-        </Route>
-      </Switch>
+              <Route path="*">
+                <NoMatch />
+              </Route>
+            </Switch>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
@@ -141,15 +151,13 @@ function FetchDefault() {
 function Header({ loggedIn, loginMsg }) {
   return (
     <ul className="header">
-      
-      
       {loggedIn && (
         <>
-        <li>
-        <NavLink activeClassName="active" to="/frontpage">
-          Home
-        </NavLink>
-      </li>
+          <li>
+            <NavLink activeClassName="active" to="/frontpage">
+              Home
+            </NavLink>
+          </li>
           <li>
             <NavLink activeClassName="active" to="/page3">
               Profile
@@ -169,14 +177,13 @@ function Header({ loggedIn, loginMsg }) {
       )}
       {!loggedIn && (
         <li>
-        <NavLink exact activeClassName="active" to="/">
-          {/* {loginMsg} */}
-          <Redirect to="/"/>
-          Login page
-        </NavLink>
-      </li>
+          <NavLink exact activeClassName="active" to="/">
+            {/* {loginMsg} */}
+            <Redirect to="/" />
+            Login page
+          </NavLink>
+        </li>
       )}
-      
     </ul>
   );
 }
@@ -189,7 +196,6 @@ function NoMatch() {
     </h3>
   );
 }
-
 
 // Can be deleted, moved to Adminpage
 
--- src/loginForm.js
+++ src/loginForm.js
@@ -1,40 +1,79 @@
 import React, { useState, useEffect } from "react";
+import { Card, Button, Form } from "react-bootstrap";
 
-function LogInForm({ login, errorMes ,setErrorMes}) {
-    const init = { username: "", password: "" };
-    const [loginCredentials, setLoginCredentials] = useState(init);
+function LogInForm({ login, errorMes, setErrorMes }) {
+  const init = { username: "", password: "" };
+  const [loginCredentials, setLoginCredentials] = useState(init);
 
-    useEffect(() => {
-        setErrorMes("");
-    }, []);
-   
-    const performLogin = (evt) => {
-      evt.preventDefault();
-      login(loginCredentials.username, loginCredentials.password)
-      
-    }
-    const onChange = (evt) => {
-      setLoginCredentials({ ...loginCredentials,[evt.target.id]: evt.target.value })
-    }
-   
-    return (
-      <div align="center">
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <h2>Welcome to our website!</h2>
-        <br/>
-        <h3>Login to continue</h3>
-        <form onChange={onChange} >
-          <input placeholder="User Name" id="username" />
-          <input placeholder="Password" id="password" />
-          <button onClick={performLogin}>Login</button>
-        </form>
-    <p>{errorMes}</p>
-      </div>
-    )
-   
-  }
+  useEffect(() => {
+    setErrorMes("");
+  }, []);
 
-  export default LogInForm;
+  const performLogin = (evt) => {
+    evt.preventDefault();
+    login(loginCredentials.username, loginCredentials.password);
+  };
+  const onChange = (evt) => {
+    setLoginCredentials({
+      ...loginCredentials,
+      [evt.target.id]: evt.target.value,
+    });
+  };
+
+  return (
+    <div>
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title className="text-center">
+            Welcome to our website!
+          </Card.Title>
+          <Card.Text className="text-center">Login to continue</Card.Text>
+          <Form onSubmit={performLogin}>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Control
+                type="email"
+                placeholder="User Name"
+                id="username"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                id="password"
+              />
+            </Form.Group>
+            <Button
+              className=""
+              variant="primary"
+              size="lg"
+              type="submit"
+              block
+            >
+              Login
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      {/*div align="center">
+          <br />
+          <br />
+          <br />
+          <br />
+          <h2>Welcome to our website!</h2>
+          <br />
+          <h3>Login to continue</h3>
+          <form onChange={onChange}>
+            <input placeholder="User Name" id="username" />
+            <input placeholder="Password" id="password" />
+            <button onClick={performLogin}>Login</button>
+          </form>
+          <p>{errorMes}</p>
+        </div>
+        */}
+    </div>
+  );
+}
+
+export default LogInForm;
