df20525bc6bfade64b843da4053aa87924b8a181
matti Hansen <MattiBSH>
2021-05-15 21:39:49 +0200

    reqs

--- src/components/ProfileMakePost.jsx
+++ src/components/ProfileMakePost.jsx
@@ -6,7 +6,6 @@ import {
   Button,Hint
 } from "react-bootstrap";
 import checkInput from "./InputChecker.js";
-import Recaptcha from 'react-recaptcha'
 
 function MakePost() {
     const [errorMes, setErrorMes] = useState("");
@@ -18,12 +17,9 @@ function MakePost() {
     const [userName, setUsername] = useState("");
     const [email, setEmail] = useState();
     const [userResult, setUserResult] = useState("");
-    const [valiCheck, setValiCheck] = useState();
-    const [verify, setVerify] = useState(false);
-    const [token,setToken]=useState("");
+  
 
-    let recaptchaInstance;
-    let token1;
+   
     useEffect(() => {
         facade.fetchDataUser()
         .then((data) => setDataFromServer(data.msg))
@@ -61,8 +57,8 @@ function MakePost() {
         const target = event.target;
         const value = target.value;
         const prop = target.id;
-        let tmpPost = { ...newPost, [prop]: value };
-        let tmp2 = { ...tmpPost, user: {token}}
+        let tmpPost = { ...newPost, [prop]: value};
+        let tmp2 = { ...tmpPost}
         setNewPost(tmp2);
     }
 
@@ -74,14 +70,15 @@ function MakePost() {
           setErrorMes("Input not allowed");
         } else {
           setErrorMes("");
-          if(verify) {
+          
+            
             facade.addPosten(newPost).catch((err) => {
             err.fullError.then((err) => {
               setErrorMes("Your post cannot be empty.");
             });
           });
           setNewPost({ content: "" });
-        }
+        
           
 
           /* setErrorMes("");
@@ -91,35 +88,17 @@ function MakePost() {
       }
     }
     
-    const resetRecaptcha = () => {
-      recaptchaInstance.reset();  
-    };
-    const verifyCallback = (response) => {
-      if(response){
-        token1=response;
-        setToken(response);
-        
-        setVerify(true);
-        
-      }
-    };
+
     return (
         <div className="info">
             <h3>Make a post</h3>
-            <div class="alert alert-primary" role="alert">
-                {valiCheck}
-              </div>
             <form>
                 <input style={{height:"200px", width:"80%"}} type="text" id="content" onChange={handleChange}/>
                 {/* <input type="text" id="user" value={userName} onChange={handleChange}/> */}
                 {/* <button onClick={handleSubmit}>Send post</button> */}
                 <br/>
                 <div>
-                <Recaptcha
-                  ref={e => recaptchaInstance = e}
-                  sitekey="6LcIFdAaAAAAAENI3sqaj5ARz7DOYMovZng2lHO3"
-                  verifyCallback={verifyCallback}
-                />
+                
                 {/* <button onClick={resetRecaptcha}>
                   Reset
                 </button> */}
