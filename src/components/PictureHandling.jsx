51ad7358a41db26d8ef72e2cee8d376fd2b09081
Joakim Skaarup Stensnæs <joakim.kss@gmail.com>
2021-05-26 06:40:26 +0200

    visual updates

--- src/components/PictureHandling.jsx
+++ src/components/PictureHandling.jsx
@@ -3,7 +3,7 @@ import "../App.css";
 import facade from "../apiFacade";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./Footer";
-import { Table, Button, Row, Form } from "react-bootstrap";
+import { Table, Button, Row, Form, Card, Image } from "react-bootstrap";
 import checkInput from "./InputChecker.js";
 
 function PictureHandling() {
@@ -36,50 +36,20 @@ function PictureHandling() {
   return (
     <div className="pictures p-2">
       <h3 align="center"> Your pictures here</h3>
-      <div className="mx-5 my-2 border border-secondary">
-        <br />
-
-        {
-          //posts.list.reverse().map((post) => {
-
-          Object.keys(pictures).map((key) => {
+      <Card className="m-2">
+        <Card.Body>
+          {Object.keys(pictures).map((key) => {
             let picture = pictures[key];
             if (picture.content) {
               return (
                 <React.Fragment>
-                  <img src={picture.content}></img>;
+                  <Image src={picture.content} rounded />;
                 </React.Fragment>
               );
             }
-          })
-        }
-
-        {/* 
-        {pictures.list && (
-          <>
-            {pictures.list.reverse().map((post) => {
-              return (
-                <div key={post.id}>
-                  <Card bg="light" border="success" style={{ width: "40rem" }}>
-                    <Card.Title as="h6">
-                      Post by {post.user.userName}
-                    </Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted" as="p">
-                      {post.posted}
-                    </Card.Subtitle>
-                    <Card.Body>
-                      <Card.Text>{post.content}</Card.Text>
-                    </Card.Body>
-                  </Card>
-                  <br />
-                </div>
-              );
-            })}
-          </>
-        )}
-*/}
-        <br />
-      </div>
+          })}
+        </Card.Body>
+      </Card>
       <div className="p-2" align="center">
         <Form>
           <div className="custom-file">
@@ -95,7 +65,9 @@ function PictureHandling() {
             <label className="custom-file-label" htmlFor="content">
               {fileName}
             </label>
-            <Button onClick={handleSubmit}>Upload</Button>
+            <Button className="m-2" onClick={handleSubmit}>
+              Upload
+            </Button>
           </div>
         </Form>
       </div>
--- src/components/ProfilePost.jsx
+++ src/components/ProfilePost.jsx
@@ -1,109 +1,121 @@
 import React, { useCallback, useEffect, useState } from "react";
 import "../App.css";
 import facade from "../apiFacade";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from './Footer'
-import {
-  Table,
-  Button,
-  Row,Card
-} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./Footer";
+import { Table, Button, Row, Card } from "react-bootstrap";
 import checkInput from "./InputChecker.js";
 
 function ProfilePage() {
   const [word, setWord] = useState("");
   const [posts, setPosts] = useState([]);
-  const [content,setContent]=useState("")
-  const [edit, setEdit]=useState(false);
-  const [id, setEditID]=useState();
-  const [delText,setDelText]=useState("");
+  const [content, setContent] = useState("");
+  const [edit, setEdit] = useState(false);
+  const [id, setEditID] = useState();
+  const [delText, setDelText] = useState("");
   const [errorMes, setErrorMes] = useState("");
- 
+
   const getByWord = () => {
     let arr = [];
-    posts.filter(x => {
+    posts.filter((x) => {
       if (x != null) {
         if (x.content.includes(word)) {
           arr.push(x);
         }
       }
-    })
+    });
     setPosts(arr);
   };
   const getAll = () => {
     setDelText("");
-    facade.fetchAllUserPosts(setPosts)
+    facade.fetchAllUserPosts(setPosts);
   };
 
   const handleChange = (event) => {
     const value = event.target.value;
-    setWord(value)
+    setWord(value);
   };
   const deletePost = (e) => {
     e.preventDefault();
     const id = e.target.id;
-    let deltext1=facade.deletePostenU(id);
-    setDelText(id+" was deleted");
+    let deltext1 = facade.deletePostenU(id);
+    setDelText(id + " was deleted");
     //setTimeout(getAll,2000);
-
   };
   const startEdit = (e) => {
     e.preventDefault();
     const id1 = Number(e.target.id);
-        setEditID(id1);
+    setEditID(id1);
     setEdit(true);
   };
   const editPost = (e) => {
     e.preventDefault();
     let checked = checkInput(content);
-    if(checked === 'Error') {
+    if (checked === "Error") {
       setErrorMes("Input not allowed");
     } else {
-      let tmpDTO={"id":id,content};
+      let tmpDTO = { id: id, content };
       facade.editPostenU(tmpDTO);
-      setTimeout(getAll,2000);
+      setTimeout(getAll, 2000);
       setEdit(false);
     }
-    
   };
-  const editOnChange=(e)=>{
-      const target = e.target; 
-      const property = target.id; 
-      const value = target.value;
-      const tmpContent = value;
-      setContent(tmpContent);
-  }
-return (
-<div className="info">
-<h3>Your Posts</h3>
-<Button onClick={getAll} className="myButton">See Posts<br /></Button>
-<Button onClick={getByWord} className="myButton">See all with word<br /></Button>
-<input type="text" value={word} onChange={handleChange} />
-<table>
-  <thead><tr><th>Content</th><th>Posted</th><th>Edit</th><th>Delete</th></tr></thead>
-  <tbody>
-    {posts && (
-      posts.map((x, idx) => {
-        return (
-          <tr key={idx}>
-            <td  className="cell1">{x.content}</td>
-            <td>{x.posted}</td>
-            <td><Button variant="warning" onClick={startEdit} id={x.id}>Edit</Button>{' '}</td>
-            <td><Button variant="danger" onClick={deletePost} id={x.id}>Delete</Button> </td>
+  const editOnChange = (e) => {
+    const target = e.target;
+    const property = target.id;
+    const value = target.value;
+    const tmpContent = value;
+    setContent(tmpContent);
+  };
+  return (
+    <div className="info">
+      <h3>Your Posts</h3>
+      <Button onClick={getAll} className="myButton">
+        See Posts
+        <br />
+      </Button>
+      <Button onClick={getByWord} className="myButton">
+        See all with word
+        <br />
+      </Button>
+      <input type="text" value={word} onChange={handleChange} />
+      <table>
+        <thead>
+          <tr>
+            <th>Content</th>
+            <th>Posted</th>
+            <th>Edit</th>
+            <th>Delete</th>
           </tr>
-        )
-      })
-    )}
-    <h1>{delText&&delText}</h1>
-  </tbody>
-</table>
-{edit && (
-        
-          
+        </thead>
+        <tbody>
+          {posts &&
+            posts.map((x, idx) => {
+              return (
+                <tr key={idx}>
+                  <td className="cell1">{x.content}</td>
+                  <td>{x.posted}</td>
+                  <td>
+                    <Button variant="warning" onClick={startEdit} id={x.id}>
+                      Edit
+                    </Button>{" "}
+                  </td>
+                  <td>
+                    <Button variant="danger" onClick={deletePost} id={x.id}>
+                      Delete
+                    </Button>{" "}
+                  </td>
+                </tr>
+              );
+            })}
+          <h1>{delText && delText}</h1>
+        </tbody>
+      </table>
+      {edit && (
         <Card border="dark" className="BlueBC">
-          <h3 style={{color:'white'}}>Edit Post: </h3>
+          <h3 style={{ color: "white" }}>Edit Post: </h3>
           <form>
-            <input type="text" disabled value={"ID: " + id&& id} />
+            <input type="text" disabled value={"ID: " + id && id} />
             <br />
             <textarea
               rows="5"
@@ -116,13 +128,12 @@ return (
             />
             <br />
             <Button onClick={editPost}>Submit change</Button>
-            <p style={{color:'white'}}>{errorMes}</p>
+            <p style={{ color: "white" }}>{errorMes}</p>
           </form>
           <br />
         </Card>
       )}
-
-</div>
+    </div>
   );
 }
 export default ProfilePage;
